9223c80b6a51dc90a155f5d868d287ba28e277ab
KaarenTenson <tensonkaaren@gmail.com>
2024-04-19 19:42:31 +0300

    failist lugemine enamvähem valmis

---
+++ src/main/java/org/example/quizrakendus/lugemine_ja_kirjutamine.java
@@ -0,0 +1,51 @@
+package org.example.quizrakendus;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class lugemine_ja_kirjutamine {
+    public static quiz loe(String projekt) throws IOException {
+        List<String[]> kaartidelist = new ArrayList<>();
+        try (DataInputStream dis = new DataInputStream(new FileInputStream(projekt+".bin"))) {
+            int arv = dis.readInt();
+            for (int i = 0; i < arv; i++) {
+                String Kusismus = dis.readUTF();
+                String Vastus = dis.readUTF();
+                kaartidelist.add(new String[]{Kusismus, Vastus});
+            }
+            return new quiz(projekt,kaartidelist,arv);
+        }
+    }
+    private static List<String> leiaprojektid(){
+        List<String> projektid = new ArrayList<>();
+        try (InputStream baidid = new FileInputStream("Projektid.txt");
+             InputStreamReader tekst = new InputStreamReader(baidid, Charset.defaultCharset());
+             BufferedReader puhverdatud = new BufferedReader(tekst)) {
+            String rida = puhverdatud.readLine();
+            while (rida != null) {
+                rida = puhverdatud.readLine(); // loeb järgmise rea. kui ei saa, tagastab nulli
+                projektid.add(rida);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return projektid;
+    }
+    private static void kirjutaprojektid(ArrayList<quiz> projektid){
+        try (
+                BufferedWriter puhverdatud = new BufferedWriter(new FileWriter("sisend.txt",Charset.defaultCharset()))) {
+            for(int i=0;i<projektid.size();i++){
+            puhverdatud.write(projektid.get(i).getNimi());}
+            // loeb järgmise rea. kui ei saa, tagastab nulli
+        }   catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        quiz esimen=new quiz("test",new ArrayList<>(),0);
+        esimen.kirjuta();
+    }
+}
---
+++ src/main/java/org/example/quizrakendus/quiz.java
@@ -0,0 +1,55 @@
+package org.example.quizrakendus;
+
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+public class quiz {
+    private String nimi;
+    private List<String[]> flashcards;
+    private int arv;
+
+    public quiz(String nimi, List<String[]>  flashcards, int arv) {
+        this.nimi = nimi;
+        this.flashcards = flashcards;
+        this.arv = arv;
+    }
+    public void kustuta(int indeks){
+        flashcards.remove(indeks);
+    }
+    public void lisa(String[] kaart){
+        flashcards.add(kaart);
+        arv+=1;
+    }
+    public void kirjuta() throws IOException {
+        try (DataOutputStream dos = new DataOutputStream(new FileOutputStream(nimi+".bin"))) {
+            dos.writeInt(arv);
+            for (String[] el:flashcards) {
+                dos.writeUTF(el[0]);
+                dos.writeUTF(el[1]);
+            }
+        }
+
+    }
+    public void muuda(int indeks, String uusKus, String uusVas){
+        this.flashcards.get(indeks)[0]=uusKus;
+        this.flashcards.get(indeks)[1]=uusVas;
+    }
+
+    public String getNimi() {
+        return nimi;
+    }
+
+    public void setNimi(String nimi) {
+        this.nimi = nimi;
+    }
+
+    public List<String[]> getFlashcards() {
+        return flashcards;
+    }
+
+    public void setFlashcards(List<String[]> flashcards) {
+        this.flashcards = flashcards;
+    }
+}
