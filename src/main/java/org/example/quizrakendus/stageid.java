15409ef8542600f161093c5c9a1be0a285fa2756
KaarenTenson <tensonkaaren@gmail.com>
2024-05-10 14:33:50 +0300

    failist lugemine enamvähem valmis

--- src/main/java/org/example/quizrakendus/stageid.java
+++ src/main/java/org/example/quizrakendus/stageid.java
@@ -46,7 +46,8 @@ public class stageid {
         TextField kusimus = new TextField();
         TextField vastus= new TextField();
         Button nupp=new Button("Salvesta");
-        Button nuppjargmine=new Button("jargmine");
+        Button nuppjargmine=new Button("järgmine");
+        Button nuppeelmine=new Button("eelmine");
         Label label = new Label("Sisesta flashkaar");
         EventHandler<ActionEvent> event2 = new EventHandler<ActionEvent>() {
             public void handle(ActionEvent e)
@@ -64,25 +65,47 @@ public class stageid {
         EventHandler<ActionEvent> event3 = new EventHandler<ActionEvent>() {
             public void handle(ActionEvent e)
             {
-                    indeks[0] +=1;
-                    if(indeks[0] <uusquiz.getFlashcards().size()){
-                        kusimus.setText(uusquiz.getFlashcards().get(indeks[0])[0]);
-                        vastus.setText(uusquiz.getFlashcards().get(indeks[0])[1]);
+                if(indeks[0] <uusquiz.getFlashcards().size()-1){
+                    uusquiz.muuda(indeks[0],kusimus.getText(),vastus.getText());
+                    kusimus.setText(uusquiz.getFlashcards().get(indeks[0]+1)[0]);
+                    vastus.setText(uusquiz.getFlashcards().get(indeks[0]+1)[1]);
 
-                    }
+                }
+                else{
                     uusquiz.lisa(new String[]{kusimus.getText(),vastus.getText()});
                     kusimus.setText("");
-                    vastus.setText("");
+                    vastus.setText("");}
+                indeks[0] +=1;
+                //stage.setScene(Avaleht.avaleht);
+            }
+        };
+        EventHandler<ActionEvent> event4 = new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e)
+            {
+                if(indeks[0]>uusquiz.getFlashcards().size()-1){
+                    uusquiz.lisa(new String[]{kusimus.getText(),vastus.getText()});
+                }
+                else{
+                uusquiz.muuda(indeks[0],kusimus.getText(),vastus.getText());}
+                if(indeks[0] >=1){
+                    indeks[0] -=1;
+                    System.out.println("aa");
+                    kusimus.setText(uusquiz.getFlashcards().get(indeks[0])[0]);
+                    vastus.setText(uusquiz.getFlashcards().get(indeks[0])[1]);
+
+                }
                 //stage.setScene(Avaleht.avaleht);
             }
         };
         nupp.setOnAction(event2);
         nuppjargmine.setOnAction(event3);
+        nuppeelmine.setOnAction(event4);
         juur.add(kusimus,1,0);
         juur.add(vastus,2,0);
         juur.add(label,1,1);
         juur.add(nupp,1,2);
         juur.add(nuppjargmine,2,2);
+        juur.add(nuppeelmine,3,2);
         Scene scene = new Scene(juur, 700, 700);
         return scene;
     }
