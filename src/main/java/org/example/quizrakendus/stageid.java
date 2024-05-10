354d86ee28a7e29a3323d8ab7011823da49a0046
KaarenTenson <tensonkaaren@gmail.com>
2024-05-10 15:16:22 +0300

    failist lugemine enamvähem valmis

--- src/main/java/org/example/quizrakendus/stageid.java
+++ src/main/java/org/example/quizrakendus/stageid.java
@@ -1,21 +1,16 @@
 package org.example.quizrakendus;
 
-import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Stack;
 
 public class stageid {
     public ArrayList<Scene> kohad;
@@ -40,7 +35,12 @@ public class stageid {
         return scene;
     }
     public static Scene lisaflash(String nimi, Stage stage){
-        quiz uusquiz=new quiz(nimi,new ArrayList<>(),0);
+        quiz uusquiz;
+        try {
+           uusquiz=lugemine_ja_kirjutamine.loe(nimi);
+        }catch (IOException e){
+            uusquiz=new quiz(nimi,new ArrayList<>(),0);
+        }
         final int[] indeks = {0}; //näitab mitmes flashcard see on
         GridPane juur=new GridPane();
         TextField kusimus = new TextField();
@@ -49,12 +49,15 @@ public class stageid {
         Button nuppjargmine=new Button("järgmine");
         Button nuppeelmine=new Button("eelmine");
         Label label = new Label("Sisesta flashkaar");
+        Label label1=new Label("1");
+        nuppeelmine.setDisable(true);
+        quiz finalUusquiz = uusquiz;
         EventHandler<ActionEvent> event2 = new EventHandler<ActionEvent>() {
             public void handle(ActionEvent e)
             {
                 Avaleht uus=new Avaleht();
                 try {
-                    uusquiz.kirjuta();
+                    finalUusquiz.kirjuta();
                     uus.start(stage);
                 } catch (IOException ex) {
                     throw new RuntimeException(ex);
@@ -65,35 +68,45 @@ public class stageid {
         EventHandler<ActionEvent> event3 = new EventHandler<ActionEvent>() {
             public void handle(ActionEvent e)
             {
-                if(indeks[0] <uusquiz.getFlashcards().size()-1){
-                    uusquiz.muuda(indeks[0],kusimus.getText(),vastus.getText());
-                    kusimus.setText(uusquiz.getFlashcards().get(indeks[0]+1)[0]);
-                    vastus.setText(uusquiz.getFlashcards().get(indeks[0]+1)[1]);
+                if(!kusimus.getText().isEmpty()){
+                if(nuppeelmine.isDisable()){
+                    nuppeelmine.setDisable(false);
+                }
+                if(indeks[0] <finalUusquiz.getFlashcards().size()-1){
+                    finalUusquiz.muuda(indeks[0],kusimus.getText(),vastus.getText());
+                    kusimus.setText(finalUusquiz.getFlashcards().get(indeks[0]+1)[0]);
+                    vastus.setText(finalUusquiz.getFlashcards().get(indeks[0]+1)[1]);
 
                 }
                 else{
-                    uusquiz.lisa(new String[]{kusimus.getText(),vastus.getText()});
+                    finalUusquiz.lisa(new String[]{kusimus.getText(),vastus.getText()});
                     kusimus.setText("");
                     vastus.setText("");}
-                indeks[0] +=1;
+                indeks[0] +=1;}
+                label1.setText(String.valueOf(indeks[0]+1));
+
                 //stage.setScene(Avaleht.avaleht);
             }
         };
         EventHandler<ActionEvent> event4 = new EventHandler<ActionEvent>() {
             public void handle(ActionEvent e)
             {
-                if(indeks[0]>uusquiz.getFlashcards().size()-1){
-                    uusquiz.lisa(new String[]{kusimus.getText(),vastus.getText()});
+                if (indeks[0]<=1){
+                        nuppeelmine.setDisable(true);
+                }
+                if(indeks[0]>finalUusquiz.getFlashcards().size()-1){
+                    finalUusquiz.lisa(new String[]{kusimus.getText(),vastus.getText()});
                 }
                 else{
-                uusquiz.muuda(indeks[0],kusimus.getText(),vastus.getText());}
+                    finalUusquiz.muuda(indeks[0],kusimus.getText(),vastus.getText());}
                 if(indeks[0] >=1){
                     indeks[0] -=1;
-                    System.out.println("aa");
-                    kusimus.setText(uusquiz.getFlashcards().get(indeks[0])[0]);
-                    vastus.setText(uusquiz.getFlashcards().get(indeks[0])[1]);
-
+                    kusimus.setText(finalUusquiz.getFlashcards().get(indeks[0])[0]);
+                    vastus.setText(finalUusquiz.getFlashcards().get(indeks[0])[1]);
                 }
+                label1.setText(String.valueOf(indeks[0]+1));
+
+
                 //stage.setScene(Avaleht.avaleht);
             }
         };
@@ -106,6 +119,7 @@ public class stageid {
         juur.add(nupp,1,2);
         juur.add(nuppjargmine,2,2);
         juur.add(nuppeelmine,3,2);
+        juur.add(label1,0,0);
         Scene scene = new Scene(juur, 700, 700);
         return scene;
     }
